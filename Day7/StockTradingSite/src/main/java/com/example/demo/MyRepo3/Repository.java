779a3ac496ef1dedf61f8a4057312867f6624574
TheBlur14 <138851236+TheBlur14@users.noreply.github.com>
2023-07-20 14:48:00 +0530

    Delete Day7/StockTradingSite directory

--- Day7/StockTradingSite/src/main/java/com/example/demo/MyRepo3/Repository.java
+++
@@ -1,9 +0,0 @@
-    package com.example.demo.MyRepo3;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.HotelBook;
-
-public interface Repository extends JpaRepository<HotelBook, Long> {
-
-}
